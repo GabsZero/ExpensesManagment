b7080eb64cbcc625b22f052c2d5bd97e2dc88295
Johnny <joaogbdesouza@gmail.com>
2019-05-25 18:08:22 -0300

    Starting Project

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManagment.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) :base(options)
+        {
+
+        }
+    }
+}
