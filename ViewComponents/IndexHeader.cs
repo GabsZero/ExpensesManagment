9ee15bca9477f582571667960126f0c041a079d9
Johnny <joaogbdesouza@gmail.com>
2019-05-28 00:49:36 -0300

    Components everywhere

---
+++ ViewComponents/IndexHeader.cs
@@ -0,0 +1,21 @@
+﻿using ExpenseManagment.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManagment.Views.Shared.Components
+{
+    public class IndexHeader : ViewComponent
+    {
+        public IViewComponentResult Invoke(string header)
+        {
+            IndexHeaderViewModel model = new IndexHeaderViewModel
+            {
+                Header = header
+            };
+            return View(model);
+        }
+    }
+}
---
+++ ViewModels/IndexHeaderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManagment.ViewModels
+{
+    public class IndexHeaderViewModel
+    {
+        public string Header { get; set; }
+    }
+}
