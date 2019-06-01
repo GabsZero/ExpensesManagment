eabeb05635939f2b586ccfc7e16b1b87bb27301d
Johnny <joaogbdesouza@gmail.com>
2019-06-01 18:55:53 -0300

    Adding income x expense chart

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -15,6 +15,32 @@ namespace ExpenseManagment.Controllers
         {
             _context = context;
         }
+
+        public async Task<JsonResult> getIncomes(int monthId)
+        {
+            var incomes = _context.Incomes.Include(e => e.IncomeType).Include(e => e.Month)
+                            .Where(m => m.MonthId == monthId)
+                            .GroupBy(e => new { e.Month, e.IncomeType })
+                            .Select(e => new {
+                                e.Key.Month,
+                                e.Key.IncomeType,
+                                Amount = e.Sum(s => s.Amount)
+                            });
+
+            var incomesDataSet = new
+            {
+                type = new List<string>(),
+                amount = new List<decimal>()
+            };
+            foreach (var income in incomes)
+            {
+                incomesDataSet.type.Add(income.IncomeType.Name);
+                incomesDataSet.amount.Add(income.Amount);
+            };
+
+            return Json(incomesDataSet);
+        }
+        
         public async Task<JsonResult> GetExpenses(int monthId)
         {
 
@@ -38,6 +64,20 @@ namespace ExpenseManagment.Controllers
             };
 
             return Json(expensesDataSet);
+        }
+
+        public async Task<JsonResult> GetMonthResult(int monthId)
+        {
+            var monthResult = _context.Months.Include(m => m.Expenses)
+                .Where(m => m.Id == monthId).Select(m => new { incomes = m.Incomes.Sum(i => i.Amount), expenses = m.Expenses.Sum(e => e.Amount) });
+
+            var monthResultDataset = new
+            {
+                expenses = monthResult.First().expenses,
+                incomes = monthResult.First().incomes
+            };
+
+            return Json(monthResultDataset);
         }
     }
 }
